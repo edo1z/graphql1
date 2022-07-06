b44b59ed08f57276a57eb7376da1d2b5dadc19fd
web3ten0 <web3ten0@gmail.com>
2022-07-06 23:08:44 +0900

    first

---
+++ server/index.ts
@@ -0,0 +1,40 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "book1",
+    author: "Taro",
+  },
+  {
+    title: "book2",
+    author: "Jiro",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: "bounded",
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
